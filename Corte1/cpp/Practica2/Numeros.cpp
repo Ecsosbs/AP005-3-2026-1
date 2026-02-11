37e8597e0d7798a82da856b715a2736f27a1208c
Ecsosbs <andymarkez@gmail.com>
2026-02-11 15:42:19 -0500

    Add files via upload

---
+++ Corte1/cpp/Practica2/Numeros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+double num1,num2, suma; 
+
+
+int main (){ 
+    
+cout<<"digito 1: ";
+cin>> num1;
+cout<<"digito2: ";
+cin>> num2;
+suma=num1+num2;
+cout<<"resultado es: "<<suma<<endl;
+return 0;
+ }
+
+
