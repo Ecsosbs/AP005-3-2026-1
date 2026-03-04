7759e946a82e808c6a864e213ce533b970cf96b9
Ecsosbs <andymarkez@gmail.com>
2026-03-04 15:24:51 -0500

    Add list of personajes_op and print statements

--- Corte1/Teoria/List/list.py
+++ Corte1/Teoria/List/list.py
@@ -1,1 +1,7 @@
-
+#################LISTAS####################
+###########################################
+personajes_op = ['Luffy', 'Zoro','Namy','Ussop','Sanji','Chopper']  #nombre de personajes
+#input()
+print(personajes_op)
+print(type(personajes_op))
+print(personajes_op[2])
